e17eb7c85cde2b49d58eaf129a017e8733021799
rishabh-108272 <rishitibest123@gmail.com>
2024-09-07 16:34:38 +0530

    Updated Changes

--- server/util.py
+++ server/util.py
@@ -36,7 +36,7 @@ def load_saved_artifacts():
         __data_columns=json.load(f)['data_columns']
         __locations=__data_columns[3:]
     global __model   
-    with open("./artifacts/bangalore_home_prices_model.pickle","rb") as f:
+    with open("../artifacts/bangalore_home_prices_model.pickle","rb") as f:
         __model=pickle.load(f)
     
 
